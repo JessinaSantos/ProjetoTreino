3d5881c29f727c31a0c7acb1b6272ffa82f15809
Jéssina Santos <jessinasantos.brandao@gmail.com>
2026-01-18 22:17:46 -0300

    Atualizando conceitos js

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
-let nome = "Jessy"
-console.log(nome)
+// const teste = {
+//   nome: "Jessy",
+//   idade: 25,
+//   status: "casada",
+// };
 
-nome = "oi"
-console.log(nome)
+// teste.idade = 26;
 
-const oi = "oi"
+// teste.teste2 = "Ola";
 
-oi = "ola"
-console.log(oi)
+// console.log(teste);
+
+// delete teste.teste2;
+// console.log(teste);
+
+// let array = ["laranja", "maçã", "uva"];
+// console.log(array[2]);
+
+// array[2] = "melancia";
+// console.log(array[2]);
+
+// let clentes = ["maria", "joão", "joaquim"];
+// clentes[3] = "joana";
+// console.log(clentes);
+
+// delete clentes[3];
+// console.log(clentes);
+
+// let produto = {
+//   nome: "suco",
+//   preço: 12,
+//   quantidade: 10,
+// };
+
+// console.log(produto.nome);
+
+// produto.preço = 15;
+// console.log(produto);
+
+// let venda = {
+//   cliente: "jessy",
+//   produto: "fruta",
+//   valor: 15,
+// };
+
+// venda.data = "18/01/2026";
+// console.log(venda);
+
+// delete venda.produto;
+// console.log(venda);
+
+let estoque = [12, 20, 10, 50];
+let novoEstoque = [...estoque];
+novoEstoque[0] = 10;
+console.log(novoEstoque);
+console.log(estoque);
