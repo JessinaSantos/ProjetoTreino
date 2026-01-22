7ba7c0d41fa9b80bbe23bdd62ba9f02884746e4d
Jéssina Santos <jessinasantos.brandao@gmail.com>
2026-01-21 22:18:11 -0300

    Exercicíos de funções

--- script.js
+++ script.js
@@ -61,12 +61,97 @@
 
 // saudacao("Jessy");
 
-let nome = "Jessy";
-let preco = 10;
-let qtd = 2;
-let total = preco * qtd;
-function teste() {
-  console.log(total);
+// let nome = "Jessy";
+// let preco = 10;
+// let qtd = 2;
+// let total = preco * qtd;
+// function teste() {
+//   console.log(total);
+// }
+
+// teste();
+
+// Faça uma função que recebe as notas das 3 provas de um aluno e calcula a média dessas. Ao
+// término do processo, devemos ter a mensagem "a média desse aluno é x" (sendo x o resultado do
+// cálculo).
+
+// function media(nota1, nota2, nota3) {
+//   let resultado = (nota1 + nota2 + nota3) / 3;
+//   return resultado;
+// }
+
+// console.log(`A média desse aluno é ${media(8, 6, 4)}`);
+
+// Imagine agora que o aluno do item anterior não atingiu a média mínima pra passar, e por isso está
+// de prova final. Faça uma nova função que calcula a média final desse aluno a partir de 4 notas, as das 3
+// primeiras provas + a nota da prova final. O cálculo deve ser o seguinte: média_final = (média_antes_da_pf + pf)/2
+
+// Você deve usar a função do exercício 1 para calcular a média final. Faça as modificações que julgar
+// necessário.
+// Ao término do processo, devemos ter a mensagem "a média final desse aluno é x" (sendo x o
+// resultado do cálculo).
+
+// function calcularMedia(nota1, nota2, nota3) {
+//   const mediaInicial = (nota1, nota2, nota3) / 3;
+//   return mediaInicial;
+// }
+
+// function calcularMediaFinal(nota1, nota2, nota3, nota4) {
+//   const media_semPF = calcularMedia(nota1, nota2, nota3);
+//   const mediaFinal = (media_semPF + nota4) / 2;
+//   console.log(`a média final do aluno é ${mediaFinal}`);
+// }
+
+// calcularMediaFinal(8, 9, 2, 4);
+
+// function calcularDesconto(valor, percentual) {
+//   let desconto = valor * (percentual / 100);
+
+//   return desconto;
+// }
+
+// function calcularPrecoFinal(valor, percentual) {
+//   let descontoIncial = calcularDesconto(valor, percentual);
+//   let valorFinal = valor - descontoIncial;
+//   return valorFinal;
+// }
+
+// function mostrarPreco(valor, percentual) {
+//   console.log(calcularPrecoFinal(valor, percentual));
+// }
+
+// mostrarPreco(100, 50);
+
+// function calcularHorasTrabalhadas(horasPorDia, diasTrabalhados) {
+//   const horasTotais = horasPorDia * diasTrabalhados;
+//   return horasTotais;
+// }
+
+// function calcularSalarioBruto(horasTotais, valorHora) {
+//   const salarioBruto = horasTotais * valorHora;
+//   return salarioBruto;
+// }
+
+// function calcularSalarioLiquido(horasPorDia, diasTrabalhados, valorHora) {
+//   const horasTotais = calcularHorasTrabalhadas(horasPorDia, diasTrabalhados);
+//   const salarioFinal = calcularSalarioBruto(horasTotais, valorHora);
+// }
+// console.log(`Salário final: R$ ${salarioFinal.toFixed(2)}`);
+// calcularSalarioLiquido(8, 22, 25);
+
+function calcularMediaSimples(n1, n2) {
+  const media = (n1 + n2) / 2;
+  return media;
 }
 
-teste();
+function calcularMediaComRecuperacao(n1, n2, recuperacao) {
+  const mediaRP = calcularMediaSimples(n1, n2);
+  const MediaFinal = (mediaRP + recuperacao) / 2;
+  if (mediaRP >= 7) {
+    console.log(mediaRP);
+  } else {
+    console.log(MediaFinal);
+  }
+}
+
+calcularMediaComRecuperacao(5, 9, 6);
